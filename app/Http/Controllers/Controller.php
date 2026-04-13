54db512013d616e902f24e52dd90b97b8d167f1d
Lucas Guilherme <lucassguilherme159@gmail.com>
2026-04-13 11:30:07 -0400

    O que é afinal o middleware

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,11 +5,4 @@ namespace App\Http\Controllers;
 abstract class Controller
 {
 
-    public function cleanUpperCaseString($string): string
-    {
-        // remover os espaçõs em branco no início e fim de uma string
-        //converter a string para uppercase (maiúsculas)
-        return strtoupper(trim($string));
-    }
-    
 }
