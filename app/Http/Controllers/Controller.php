14f2d6993fc03b2c57e5001a3806a435521f5b04
Lucas Guilherme <lucassguilherme159@gmail.com>
2026-04-13 10:55:30 -0400

    Para que serve o controller criado pelo laravel

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,12 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
+
+    public function cleanUpperCaseString($string): string
+    {
+        // remover os espaçõs em branco no início e fim de uma string
+        //converter a string para uppercase (maiúsculas)
+        return strtoupper(trim($string));
+    }
     
 }
