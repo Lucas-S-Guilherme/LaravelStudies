981f95ebb9c4836056fcb1a81005e2e691f2e8e5
Lucas da Silva Guilherme <lucassguilherme159@gmail.com>
2026-05-06 12:00:33 -0400

    @if @elseif @else @endif

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,31 @@
 @extends('layouts/main_layout')
 @section('content')
 
-{{-- comentário de blade --}}
-<p> {{ $name }}</p>
+{{-- Intrução IF simples --}}
+@if($value == 100)
+    <h1>Primeira Parte do Código</h1>
+@endif
 
+{{-- Instrução IF ELSE --}}
+@if($value != 100)
+    <h1>Primeira Parte do Código</h1>
+@else
+    <h1>Segunda Parte do Código</h1>
+@endif
+
+{{-- instrução IF ELSEIF ELSE ENDIF --}}
+
+@if($value < 10)
+    <p>Primeiro</p>
+@elseif($value < 20)
+    <p>Segundo</p>
+@elseif($value > 50)
+    <p>Terceiro</p>
+@elseif($value == 100)
+    <p>Quarto</p>
+@else
+    <p>Outro Caso</p>
+@endif
 
 @endsection
 
