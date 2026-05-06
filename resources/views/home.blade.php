e9013ea02223e74ecfdb174c5582387df2b3e230
Lucas da Silva Guilherme <lucassguilherme159@gmail.com>
2026-05-06 12:04:39 -0400

    @switch @case @break @default @endswitch

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,31 +1,21 @@
 @extends('layouts/main_layout')
 @section('content')
 
-{{-- Intrução IF simples --}}
-@if($value == 100)
-    <h1>Primeira Parte do Código</h1>
-@endif
+{{-- switch --}}
+@switch($value)
+    @case(100)
+        <h1>Valor 100</h1>
+        @break
+    @case(200)
+        <h1>Valor 200</h1>
+        @break
+    @case(300)
+        <h1>Valor 300</h1>
+        @break
+    @default
+        <h1>Outro!</h1>
+@endswitch
 
-{{-- Instrução IF ELSE --}}
-@if($value != 100)
-    <h1>Primeira Parte do Código</h1>
-@else
-    <h1>Segunda Parte do Código</h1>
-@endif
-
-{{-- instrução IF ELSEIF ELSE ENDIF --}}
-
-@if($value < 10)
-    <p>Primeiro</p>
-@elseif($value < 20)
-    <p>Segundo</p>
-@elseif($value > 50)
-    <p>Terceiro</p>
-@elseif($value == 100)
-    <p>Quarto</p>
-@else
-    <p>Outro Caso</p>
-@endif
 
 @endsection
 
