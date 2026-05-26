c27532a1a9a6f548a1db3dbc640c4f1e45b59ac4
Lucas da Silva Guilherme <lucassguilherme159@gmail.com>
2026-05-26 12:02:54 -0400

    finalizado projeto mathx

---
+++ anotacoesAula.md
@@ -0,0 +1,24 @@
+103. gerar exercicios
+
+Validar para que numero 2 não seja maior que o 1.
+
+pegar operações selecionadas.
+
+
+104.
+ Arrumar operações vazias, sinais de operações.
+
+ Não feito.
+
+
+105.
+Pegar código operations
+
+criar view operations
+
+106.
+Escrever código dinâmico.
+
+criar um for each dentro da div class row
+
+107.
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MainController::class, 'home'])->name('home');
-Route::post('/make-exercises', [MainController::class, 'makeExercises'])->name('makeExercises');
+Route::post('/generate-exercises', [MainController::class, 'generateExercises'])->name('generateExercises');
 Route::get('/print-exercises', [MainController::class, 'printExercises'])->name('printExercises');
 Route::get('/export-exercises', [MainController::class, 'exportExercises'])->name('exportExercises');
 
